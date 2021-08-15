de3effda1861d41dced1cf7b084de4bd76de39a8
BuildTools <unconfigured@null.spigotmc.org>
2021-08-15 07:58:37 +0200

    Fixed Bitwarden import

--- js/export.js
+++ js/export.js
@@ -189,7 +189,8 @@ function import_bitwarden(){
     let passwords = [];
     for(let i = 0, j = 0; i < ido["items"].length; i++){
         if(ido["items"][i]["type"] != 1) continue;
-
+        if(typeof(ido["items"][i]["login"]["uris"]) == 'undefined') continue;
+        
         let website = ido["items"][i]["login"]["uris"][0]["uri"].replace("http://", "").replace("https://", "").replace("www.", "");
         if(website.slice(-1) == '/') website = website.slice(0, -1);
         let username = ido["items"][i]["login"]["username"];
