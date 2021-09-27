6a553c7807678b64a85a404edd18ba4d99adde35
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 09:09:00 +0200

    Prevent users to access inspect elements

--- js/header.js
+++ js/header.js
@@ -18,3 +18,13 @@ switch(localStorage.theme){
         document.getElementById("css-theme").href = "css/themes/dracula.css";
     break;
 }
+
+document.onkeydown = function(e) {
+    if(e.key == "F12") return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'I') return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'C') return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'J') return false;
+    if(e.ctrlKey && (e.key == 'u' || e.key == 'U')) return false;
+}
+
+document.addEventListener('contextmenu', e => e.preventDefault());
