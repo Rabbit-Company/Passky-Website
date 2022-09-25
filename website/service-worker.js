3f4053aa5ae223a7e34774d61203fadbc4df6060
Ziga <ziga.zajc007@gmail.com>
2022-09-25 14:49:42 +0200

    Updated service worker

--- website/service-worker.js
+++ website/service-worker.js
@@ -2,7 +2,7 @@
 
 const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
