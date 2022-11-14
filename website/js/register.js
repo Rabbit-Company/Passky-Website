d4d999cfbef1152977bd688f95484429b73914aa
Ziga <ziga.zajc007@gmail.com>
2022-11-14 13:59:20 +0100

    Added loader to register

--- website/js/register.js
+++ website/js/register.js
@@ -16,8 +16,6 @@ loadData().then(() => {
 	document.getElementById("email").placeholder = lang["email"];
 	document.getElementById("password").placeholder = lang["password"];
 	document.getElementById("tos").innerText = lang["terms_of_service"];
-	document.getElementById("btn-dialog").innerText = lang["okay"];
-	document.getElementById("error-dialog-modal-title").innerText = lang["error"];
 	document.getElementById("btn_signup").innerText = lang["signup"];
 	document.getElementById("btn_signin").innerText = lang["signin"];
 
@@ -26,10 +24,6 @@ loadData().then(() => {
 document.getElementById("signup-form").addEventListener("submit", e => {
 	e.preventDefault();
 	onBtnClick();
-});
-
-document.getElementById("btn-dialog").addEventListener("click", () => {
-	hide('error-dialog');
 });
 
 document.getElementById("btn_signin").addEventListener("click", () => {
@@ -70,6 +64,49 @@ function toggleServerPicker(){
 	}
 }
 
+function changeDialog(style, text){
+	switch(style){
+		case 1:
+			//Error dialog
+			document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10";
+			document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-red-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z' /></svg>";
+
+			document.getElementById('dialog-title').innerText = lang["error"];
+			document.getElementById('dialog-text').innerText = text;
+
+			document.getElementById('dialog-button-cancel').style.display = 'none';
+
+			document.getElementById('dialog-button').className = "dangerButton inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium focus:outline-none sm:w-auto sm:text-sm";
+			document.getElementById('dialog-button').innerText = lang["okay"];
+			document.getElementById('dialog-button').onclick = () => hide("dialog");
+		break;
+		case 2:
+			//Success dialog
+			document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10";
+			document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-green-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 13l4 4L19 7' /></svg>";
+
+			document.getElementById('dialog-title').innerText = lang["success"];
+			document.getElementById('dialog-text').innerText = text;
+
+			document.getElementById('dialog-button-cancel').style.display = 'none';
+
+			document.getElementById('dialog-button').className = "successButton inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium focus:outline-none sm:w-auto sm:text-sm";
+			document.getElementById('dialog-button').innerText = lang["okay"];
+			document.getElementById('dialog-button').onclick = () => window.location.href = 'index.html';
+		break;
+		case 3:
+			//Loading...
+			document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10";
+			document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-blue-600 animate-spin' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' aria-hidden='true'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M12 3a9 9 0 1 0 9 9'></path></svg>";
+
+			document.getElementById('dialog-title').innerText = lang["please_wait"];
+			document.getElementById('dialog-text').innerHTML = lang[text];
+
+			hideDialogButtons();
+		break;
+	}
+}
+
 function onBtnClick(){
 
 	let url = document.getElementById("passky-server").value;
@@ -82,60 +119,55 @@ function onBtnClick(){
 	}
 
 	if(PasswordEntropy.calculate(password) < 75){
-		setText('error-dialog-modal-text', lang["5"]);
-		show('error-dialog');
+		changeDialog(1, lang["5"]);
+		show('dialog');
 		return;
 	}
 
+	changeDialog(3, "signing_up");
+	show('dialog');
+
 	Passky.createAccount(url, username, password, email).then(response => {
 
+		showDialogButtons();
+
 		if(typeof response['error'] === 'undefined'){
-			setText('error-dialog-modal-text', lang["server_unreachable"]);
-			show('error-dialog');
+			changeDialog(1, lang["server_unreachable"]);
 			return;
 		}
 
 		if(response['error'] != 0){
-			setText('error-dialog-modal-text', lang[response['error']]);
-			show('error-dialog');
+			changeDialog(1, lang[response['error']]);
 			return;
 		}
 
 		writeData('url', url);
 		writeData('username', username);
 
-		setText('error-dialog-modal-title', lang["success"]);
-		setText('error-dialog-modal-text', lang["registration_completed"]);
-		document.getElementById('dialog-icon').className = "mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100";
-		document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-green-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 13l4 4L19 7' /></svg>";
-		document.getElementById('btn-dialog').className = "successButton inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium sm:text-sm"
-		document.getElementById('btn-dialog').onclick = function(){
-			window.location.href = 'index.html';
-		}
-		show('error-dialog');
+		changeDialog(2, lang["registration_completed"]);
 
 	}).catch(err => {
+		showDialogButtons();
 		switch(err){
 			case 1000:
-				setText('error-dialog-modal-text', lang["server_unreachable"]);
+				changeDialog(1, lang["server_unreachable"]);
 			break;
 			case 1001:
-				setText('error-dialog-modal-text', lang["url_invalid"]);
+				changeDialog(1, lang["url_invalid"]);
 			break;
 			case 1005:
-				setText('error-dialog-modal-text', lang["12"]);
+				changeDialog(1, lang["12"]);
 			break;
 			case 1006:
-				setText('error-dialog-modal-text', lang["5"]);
+				changeDialog(1, lang["5"]);
 			break;
 			case 1007:
-				setText('error-dialog-modal-text', lang["6"]);
+				changeDialog(1, lang["6"]);
 			break;
 			default:
-				setText('error-dialog-modal-text', lang[err]);
+				changeDialog(1, lang[err]);
 			break;
 		}
-		show('error-dialog');
 	});
 
 }
