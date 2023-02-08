4c82f19c62108c9f652e620d9c2b7a24ce8cc3e9
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2023-02-08 15:37:14 +0300

    Always force gulp to regenerate integrity

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
 const sriHash = require('gulp-sri-hash');
+const { exec } = require('child_process');
+
+/* remove all integrity and crossorigin tags from *.html file in website directory */
+exec(`
+for file in website/*.html; do
+	sed 's/ integrity="[^"]*"//g' $file | sed 's/ crossorigin="[^"]*"//g' > "$file.tmp" && mv "$file.tmp" $file
+done
+`);
 
 exports.default = function() {
 	return gulp.src('./**/*.html')
@@ -8,8 +16,10 @@ exports.default = function() {
 			algo: 'sha512',         // use strong hashing
 			// prefix: '/assets',      // no trailing slash
 			// selector: 'link[href]', // limit selector
+			cacheParser: false,			// no cache
 			relative: true          // assets reside relative to html file
 		}))
 		// ... manipulating html files further, is perfectly fine
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./')) // output
+		;
 }
