47693576d5d1fd5c6b5af32b985fba45b3ac0056
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2023-02-09 09:43:23 +0300

    Eliminate inline styles and protect `<script>` & `<style>` elements inside `html` files with CSP Hash

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const sriHash = require('gulp-sri-hash');
 const { exec } = require('child_process');
+const hashstream = require('inline-csp-hash');
 
-/* remove all integrity and crossorigin tags from *.html file in website directory */
+/* remove all integrity and crossorigin tags from *.html files in website directory */
 exec(`
 for file in website/*.html; do
 	sed 's/ integrity="[^"]*"//g' $file | sed 's/ crossorigin="[^"]*"//g' > "$file.tmp" && mv "$file.tmp" $file
@@ -19,6 +20,16 @@ exports.default = function() {
 			cacheParser: false,			// no cache
 			relative: true          // assets reside relative to html file
 		}))
+		.pipe(hashstream({
+      what: 'script',
+			hash: 'sha512', 	// use strong hashing
+      replace_cb: (s, hashes) => s.replace(/script-src localhost:\* https:[^;]*/, "script-src localhost:* https: " + hashes.join(" "))
+    }))
+    .pipe(hashstream({
+      what: 'style',
+			hash: 'sha512', 	// use strong hashing
+      replace_cb: (s, hashes) => s.replace(/style-src localhost:\* https:[^;]*/, "style-src localhost:* https: " + hashes.join(" "))
+    }))
 		// ... manipulating html files further, is perfectly fine
 		.pipe(gulp.dest('./')) // output
 		;
